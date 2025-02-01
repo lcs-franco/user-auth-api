ccf1d12636f6404988f289a1601d9e79ca376808
Lucas Franco <lcs.franco556@gmail.com>
2025-02-01 16:04:24 -0300

    feat: create sign in use case

---
+++ src/app/useCases/SignInUseCase.ts
@@ -0,0 +1,34 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { env } from "../config/env";
+import { InvalidCredentials } from "../errors/InvalidCredentials";
+import { prismaClient } from "../lib/prismaClient";
+
+interface IInput {
+  email: string;
+  password: string;
+}
+
+interface IOutput {
+  accessToken: string;
+}
+
+export class SignInUseCase {
+  async execute({ email, password }: IInput): Promise<IOutput> {
+    const account = await prismaClient.account.findUnique({
+      where: { email },
+    });
+
+    if (!account) throw new InvalidCredentials();
+
+    const isPasswordValid = await compare(password, account.password);
+
+    if (!isPasswordValid) throw new InvalidCredentials();
+
+    const accessToken = sign({ sub: account.id }, env.jwtSecret, {
+      expiresIn: "1d",
+    });
+
+    return { accessToken };
+  }
+}
