cd9bd2b595bdda2a381e8b2de176e7b1ccf40f0f
Lucas Franco <lcs.franco556@gmail.com>
2025-02-02 14:59:00 -0300

    refactor: implements route adapter to server routes

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,30 +1,12 @@
-import Fastify, { FastifyRequest } from "fastify";
+import Fastify from "fastify";
 import { makeSignInController } from "../factories/makeSignInController";
 import { makeSignUpController } from "../factories/makeSignUpController";
+import { routeAdapter } from "./adapters/routeAdapter";
 
 const fastify = Fastify();
 
-type Request = FastifyRequest<{ Body: Record<string, any> }>;
-
-fastify.post("/sign-up", async (request: Request, reply) => {
-  const signUpController = makeSignUpController();
-
-  const { body, statusCode } = await signUpController.handle({
-    body: request.body,
-  });
-
-  return reply.code(statusCode).send(body);
-});
-
-fastify.post("/sign-in", async (request: Request, reply) => {
-  const signInController = makeSignInController();
-
-  const { body, statusCode } = await signInController.handle({
-    body: request.body,
-  });
-
-  return reply.code(statusCode).send(body);
-});
+fastify.post("/sign-up", routeAdapter(makeSignUpController()));
+fastify.post("/sign-in", routeAdapter(makeSignInController()));
 
 export async function main() {
   try {
