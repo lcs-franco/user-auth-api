9db4bf084a4a5abb166bc764f69f2848beee6517
Lucas Franco <lcs.franco556@gmail.com>
2025-02-02 12:10:40 -0300

    feat: create sign up route

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,6 +4,20 @@ import { SignUpUseCase } from "../app/useCases/SignUpUseCase";
 
 const fastify = Fastify();
 
+type Request = FastifyRequest<{ Body: Record<string, any> }>;
+
+fastify.post("/sign-up", async (request: Request, reply) => {
+  const SALT = 10;
+  const signUpUseCase = new SignUpUseCase(SALT);
+  const signUpController = new SignUpController(signUpUseCase);
+
+  const { body, statusCode } = await signUpController.handle({
+    body: request.body,
+  });
+
+  return reply.code(statusCode).send(body);
+});
+
 export async function main() {
   try {
     fastify.listen({ port: 3001 }, () => {
