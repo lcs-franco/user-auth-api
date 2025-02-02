e1c986ab9c431f47cc7741836e3678c588906473
Lucas Franco <lcs.franco556@gmail.com>
2025-02-02 14:23:48 -0300

    feat: create sign in route

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import Fastify, { FastifyRequest } from "fastify";
+import { SignInContoller } from "../app/controllers/SignInController";
 import { SignUpController } from "../app/controllers/SignUpController";
+import { SignInUseCase } from "../app/useCases/SignInUseCase";
 import { SignUpUseCase } from "../app/useCases/SignUpUseCase";
 
 const fastify = Fastify();
@@ -12,6 +14,17 @@ fastify.post("/sign-up", async (request: Request, reply) => {
   const signUpController = new SignUpController(signUpUseCase);
 
   const { body, statusCode } = await signUpController.handle({
+    body: request.body,
+  });
+
+  return reply.code(statusCode).send(body);
+});
+
+fastify.post("/sign-in", async (request: Request, reply) => {
+  const signInUseCase = new SignInUseCase();
+  const signInController = new SignInContoller(signInUseCase);
+
+  const { body, statusCode } = await signInController.handle({
     body: request.body,
   });
 
