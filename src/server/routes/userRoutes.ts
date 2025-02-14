bf30a936d9dccbf636f066eb9bdb331e7809c9f6
Lucas Franco <lcs.franco556@gmail.com>
2025-02-14 16:18:37 -0300

    feat: create a userRoutes to better structure app

---
+++ src/server/routes/userRoutes.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance } from "fastify";
+import { makeSignInController } from "../../factories/makeSignInController";
+import { makeSignUpController } from "../../factories/makeSignUpController";
+import { routeAdapter } from "../adapters/routeAdapter";
+
+export async function userRoutes(fasfity: FastifyInstance) {
+  fasfity.post("/sign-up", routeAdapter(makeSignUpController()));
+  fasfity.post("/sign-in", routeAdapter(makeSignInController()));
+
+  fasfity.post("/logout", () => {});
+}
