ea0e069dbc4acb020573326bc5c1255083f1623a
Lucas Franco <lcs.franco556@gmail.com>
2025-02-16 00:26:06 -0300

    feat: create middleware adapter to imput sub of jwt on request metadata

---
+++ src/server/adapters/middlewareAdapter.ts
@@ -0,0 +1,26 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { IMiddleware } from '../../app/interfaces/IMiddleware';
+
+type Request = FastifyRequest<{
+  Body: Record<string, any>;
+  Params: Record<string, string>;
+}>;
+
+type Reply = FastifyReply;
+
+export function middlewareAdapter(middleware: IMiddleware) {
+  return async (request: Request, reply: Reply) => {
+    const result = await middleware.handle({
+      headers: request.headers as Record<string, string>,
+    });
+
+    if ('statusCode' in result) {
+      return reply.code(result.statusCode).send(result.body);
+    }
+
+    request.metadata = {
+      ...request.metadata,
+      ...result.data,
+    };
+  };
+}
