b6ded6d9b3fe5c916e00ac09dc402a4f228acaee
Lucas Franco <lcs.franco556@gmail.com>
2025-02-16 00:27:44 -0300

    feat: import adapter and factory related of authentication on list users pre handler

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,20 +1,18 @@
-import Fastify from "fastify";
-import { makeListUsersController } from "../factories/makeListUsersController";
-import { routeAdapter } from "./adapters/routeAdapter";
-import { userRoutes } from "./routes/userRoutes";
+import Fastify from 'fastify';
+import { makeAuthenticationMiddleware } from '../factories/makeAuthenticationMiddleware';
+import { makeListUsersController } from '../factories/makeListUsersController';
+import { middlewareAdapter } from './adapters/middlewareAdapter';
+import { routeAdapter } from './adapters/routeAdapter';
+import { userRoutes } from './routes/userRoutes';
 
 const fastify = Fastify();
 
-fastify.register(userRoutes, { prefix: "users" });
+fastify.register(userRoutes, { prefix: 'users' });
 
 fastify.get(
-  "/list-users",
+  '/list-users',
   {
-    preHandler: (request, reply, done) => {
-      const authorization = request.headers.authorization;
-      if (!authorization) return reply.code(401).send({});
-      done();
-    },
+    preHandler: [middlewareAdapter(makeAuthenticationMiddleware())],
   },
   routeAdapter(makeListUsersController())
 );
@@ -22,7 +20,7 @@ fastify.get(
 export async function main() {
   try {
     fastify.listen({ port: 3001 }, () => {
-      console.log("> server started at http://localhost:3001");
+      console.log('> server started at http://localhost:3001');
     });
   } catch (error) {
     console.log(error);
