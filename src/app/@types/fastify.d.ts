d218a07bf735574fa33fd537597c25ccc7a34d5f
Lucas Franco <lcs.franco556@gmail.com>
2025-02-18 22:13:58 -0300

    feat: passed role on metadata type

--- src/app/@types/fastify.d.ts
+++ src/app/@types/fastify.d.ts
@@ -3,7 +3,10 @@ import 'fastify';
 declare module 'fastify' {
   export interface FastifyRequest {
     metadata?: {
-      accountId?: string;
+      account?: {
+        accountId: string;
+        role: string;
+      };
     };
   }
 }
