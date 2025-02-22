4198cf917d51786f2490ce2e2717195afd405fc8
Lucas Franco <lcs.franco556@gmail.com>
2025-02-22 20:11:21 -0300

    feat: create get roles permissions use case

---
+++ src/app/useCases/GetRolesPermissionsUseCase.ts
@@ -0,0 +1,24 @@
+import { prismaClient } from '../lib/prismaClient';
+
+interface IInput {
+  roleId: string;
+}
+
+interface IOutput {
+  permissionsCodes: string[];
+}
+
+export class GetRolesPermissionsUseCase {
+  async execute({ roleId }: IInput): Promise<IOutput> {
+    const rolePermissions = await prismaClient.rolePermission.findMany({
+      where: { roleId },
+      select: { permissionCode: true },
+    });
+
+    const permissionsCodes = rolePermissions.map(
+      (rolePermissions) => rolePermissions.permissionCode
+    );
+
+    return { permissionsCodes };
+  }
+}
