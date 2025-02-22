423f0920f25633547ed0b6e5d9eecb3a651f838c
Lucas Franco <lcs.franco556@gmail.com>
2025-02-21 21:34:00 -0300

    refactor: receive the role id on sign up

--- src/app/useCases/SignUpUseCase.ts
+++ src/app/useCases/SignUpUseCase.ts
@@ -6,6 +6,7 @@ interface IInput {
   name: string;
   email: string;
   password: string;
+  roleId: string;
 }
 
 type IOutput = void;
@@ -13,7 +14,7 @@ type IOutput = void;
 export class SignUpUseCase {
   constructor(private readonly salt: number) {}
 
-  async execute({ email, name, password }: IInput): Promise<IOutput> {
+  async execute({ email, name, password, roleId }: IInput): Promise<IOutput> {
     const accountAlreadyExists = await prismaClient.account.findUnique({
       where: { email },
     });
@@ -27,7 +28,7 @@ export class SignUpUseCase {
         email,
         name,
         password: hashedPassword,
-        role: 'USER',
+        roleId,
       },
     });
   }
