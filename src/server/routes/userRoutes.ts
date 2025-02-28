463804e4d394761efbb2eca964b44f6e75e660e7
Lucas Franco <lcs.franco556@gmail.com>
2025-02-27 22:55:22 -0300

    feat: update account route

--- src/server/routes/userRoutes.ts
+++ src/server/routes/userRoutes.ts
@@ -1,11 +1,26 @@
+import { makeAuthenticationMiddleware } from '@factories/makeAuthenticationMiddleware';
+import { makeAuthorizationMiddleware } from '@factories/makeAuthorizationMiddleware';
 import { makeSignInController } from '@factories/makeSignInController';
 import { makeSignUpController } from '@factories/makeSignUpController';
+import { makeUpdateAccountController } from '@factories/makeUpdateUsersController';
+import { middlewareAdapter } from '@server/adapters/middlewareAdapter';
 import { FastifyInstance } from 'fastify';
 import { routeAdapter } from '../adapters/routeAdapter';
 
 export async function userRoutes(fasfity: FastifyInstance) {
   fasfity.post('/sign-up', routeAdapter(makeSignUpController()));
   fasfity.post('/sign-in', routeAdapter(makeSignInController()));
+
+  fasfity.put(
+    '/',
+    {
+      preHandler: [
+        middlewareAdapter(makeAuthenticationMiddleware()),
+        middlewareAdapter(makeAuthorizationMiddleware(['accounts:update'])),
+      ],
+    },
+    routeAdapter(makeUpdateAccountController())
+  );
 
   fasfity.post('/logout', () => {});
 }
