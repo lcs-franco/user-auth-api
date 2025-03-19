d82ed6d52cf5ae685d64f52db6c462eb4eae685a
Lucas Franco <lcs.franco556@gmail.com>
2025-03-18 23:08:47 -0300

    feat: create update permission route

--- src/server/routes/permissionsRoutes.ts
+++ src/server/routes/permissionsRoutes.ts
@@ -1,6 +1,7 @@
 import { makeAuthenticationMiddleware } from '@factories/middlewares/makeAuthenticationMiddleware';
 import { makeCreatePermissionController } from '@factories/permissions/makeCreatePermissionController';
 import { makeListPermissionController } from '@factories/permissions/makeListPermissionController';
+import { makeUpdatePermissionController } from '@factories/permissions/makeUpdatePermissionController';
 import { middlewareAdapter } from '@server/adapters/middlewareAdapter';
 import { FastifyInstance } from 'fastify';
 import { routeAdapter } from '../adapters/routeAdapter';
@@ -13,4 +14,5 @@ export async function permissionsRoutes(fastify: FastifyInstance) {
 
   fastify.post('/', routeAdapter(makeCreatePermissionController()));
   fastify.get('/', routeAdapter(makeListPermissionController()));
+  fastify.put('/:id', routeAdapter(makeUpdatePermissionController()));
 }
