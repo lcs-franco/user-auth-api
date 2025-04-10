92dfebeabfe567655b9532056ceda9efed031239
Lucas Franco <lcs.franco556@gmail.com>
2025-04-09 22:32:55 -0300

    feat: remove updata account end point

--- src/factories/accounts/makeUpdateUsersController.ts
+++
@@ -1,7 +0,0 @@
-import { UpdateAccountController } from '@app/controllers/accounts/UpdateAccountController';
-import { makeUpdateAccountUseCase } from './makeUpdateUsersUseCase';
-
-export function makeUpdateAccountController() {
-  const updateAccountUseCase = makeUpdateAccountUseCase();
-  return new UpdateAccountController(updateAccountUseCase);
-}
--- src/factories/accounts/makeUpdateUsersUseCase.ts
+++
@@ -1,5 +0,0 @@
-import { UpdateAccountUseCase } from '@app/useCases/accounts/UpdateAccountUseCase';
-
-export function makeUpdateAccountUseCase() {
-  return new UpdateAccountUseCase();
-}
