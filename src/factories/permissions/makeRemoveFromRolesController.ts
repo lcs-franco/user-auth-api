10af3b3c186967c8cd968e6dbd7ef34d76c3e8ef
Lucas Franco <lcs.franco556@gmail.com>
2025-04-11 22:06:28 -0300

    feat: delete services related to delete role permission of permissions

--- src/factories/permissions/makeRemoveFromRolesController.ts
+++
@@ -1,9 +0,0 @@
-
-import { RemoveFromRolesController } from '@app/controllers/permissions/RemoveFromRolesController';
-import { makeRemoveFromRolesUseCase } from './makeRemoveFromRolesUseCase';
-
-export function makeRemoveFromRolesController() {
-  const removeFromRolesUseCase = makeRemoveFromRolesUseCase();
-  return new RemoveFromRolesController(removeFromRolesUseCase);
-}
-
