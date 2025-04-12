bb71007ef15f6605f8434a220cff10cbe8cb306a
Lucas Franco <lcs.franco556@gmail.com>
2025-04-11 21:54:56 -0300

    feat: remove services related to role permissions of permissions

--- src/factories/permissions/makeAddIntoRolesController.ts
+++
@@ -1,9 +0,0 @@
-
-import { AddIntoRolesController } from '@app/controllers/permissions/AddIntoRolesController';
-import { makeAddIntoRolesUseCase } from './makeAddIntoRolesUseCase';
-
-export function makeAddIntoRolesController() {
-  const addIntoRolesUseCase = makeAddIntoRolesUseCase();
-  return new AddIntoRolesController(addIntoRolesUseCase);
-}
-
