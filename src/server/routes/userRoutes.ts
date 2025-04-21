7650e177dc0526b9fa87287934b9bc594f4d433d
Lucas Franco <lcs.franco556@gmail.com>
2025-04-21 18:09:28 -0300

    feat: change user role end point

--- src/server/routes/userRoutes.ts
+++ src/server/routes/userRoutes.ts
@@ -2,8 +2,23 @@ import { makeSignInController } from '@factories/users/makeSignInController';
 import { makeSignUpController } from '@factories/users/makeSignUpController';
 import { FastifyInstance } from 'fastify';
 import { routeAdapter } from '../adapters/routeAdapter';
+import { makeChangeUserRoleController } from '@factories/users/makeChangeUserRoleController';
+import { makeAuthenticationMiddleware } from '@factories/middlewares/makeAuthenticationMiddleware';
+import { makeAuthorizationMiddleware } from '@factories/middlewares/makeAuthorizationMiddleware';
+import { middlewareAdapter } from '@server/adapters/middlewareAdapter';
 
 export async function userRoutes(fastify: FastifyInstance) {
   fastify.post('/sign-up', routeAdapter(makeSignUpController()));
   fastify.post('/sign-in', routeAdapter(makeSignInController()));
+
+  fastify.put(
+    '/change-role/:id',
+    {
+      preHandler: [
+        middlewareAdapter(makeAuthenticationMiddleware()),
+        middlewareAdapter(makeAuthorizationMiddleware(['role:update'])),
+      ],
+    },
+    routeAdapter(makeChangeUserRoleController())
+  );
 }
